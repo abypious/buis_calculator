c1bb52d8040002075930704e64771e229afd86ec
aby pious <abypious3@gmail.com>
2024-04-19 23:00:24 +0530

    added main function

--- pycalc/main.py
+++ pycalc/main.py
@@ -2,27 +2,41 @@ import kivy
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.config import Config
-from kivy.core.window import Window
 from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen
 
+kivy.require('1.9.0')
 
-kivy .require('1.9.0')
-
-Config.set('graphics', 'width', '80')
-Config.set('graphics', 'height', '400')
+Config.set('graphics', 'width', '400')
+Config.set('graphics', 'height', '600')
 
 class CalcGridlayout(GridLayout):
-    def calculate(self,calculation):
+    def calculate(self, calculation):
         if calculation:
             try:
-                self.display.text=str(eval(calculation))
+                self.display.text = str(eval(calculation))
             except Exception:
-                self.display.text='Error'
+                self.display.text = 'Error'
+
+class CalculatorScreen(Screen):
+    def __init__(self, **kwargs):
+        super(CalculatorScreen, self).__init__(**kwargs)
+        layout = GridLayout(cols=1)
+        layout.add_widget(CalcGridlayout())
+        self.add_widget(layout)
+
+class MainScreen(Screen):
+    pass
 
 class CalConApp(App):
     def build(self):
-        return CalcGridlayout()
+        sm = ScreenManager()
+        sm.add_widget(MainScreen(name='main'))
+        sm.add_widget(CalculatorScreen(name='calculator'))
+        return sm
 
-calcApp = CalConApp()
-calcApp.run()
+    def switch_to_calculator(self):
+        self.root.current = 'calculator'
 
+if __name__ == '__main__':
+    CalConApp().run()
