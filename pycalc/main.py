4feead8c6b19da438d2ebf507f626cd829caf534
Ashwin Joseph <ashwinjoseph2017@gmail.com>
2024-04-26 18:17:27 +0530

    ui_updated

--- pycalc/main.py
+++ pycalc/main.py
@@ -1,22 +1,39 @@
 import kivy
+import math
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.config import Config
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
+from kivy.graphics import RoundedRectangle
 
 kivy.require('1.9.0')
 
-Window.size=800,600
+Window.size=393,750
+
 
 class CalcGridlayout(GridLayout):
+
+   #def textentry(self,entry):
+    #    if entry:
+     #       try:
+      #          self.display.text += self.text
+            #except Exception:
+            #    self.display.text='Error'    
+            
+
+    def update_display(self, text):
+        self.display.text += text
+
+
     def calculate(self, calculation):
         if calculation:
             try:
                 self.display.text = str(eval(calculation))
             except Exception:
-                self.display.text = 'Error'
+                self.display.text = 'Error' 
+                   
 
 class CalculatorScreen(Screen):
     def __init__(self, **kwargs):
@@ -24,6 +41,9 @@ class CalculatorScreen(Screen):
         layout = GridLayout(cols=1)
         layout.add_widget(CalcGridlayout())
         self.add_widget(layout)
+
+
+       
 
 class MainScreen(Screen):
     pass
