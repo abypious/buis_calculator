19613a0902cd88cd984b96c012d3e40ca6216960
Ashwin Joseph <ashwinjoseph2017@gmail.com>
2024-04-28 00:20:26 +0530

    Home_page_updated

--- pycalc/main.py
+++ pycalc/main.py
@@ -18,11 +18,16 @@ Window.size=393,750
 
 
 Builder.load_file('LenConverter.kv')
+Builder.load_file('Currency.kv')
 
+class Currency_kv(GridLayout): 
+    pass
 
 class CalCon_kv(GridLayout): 
     pass
 
+
+#Calcualator class
 class CalcGridlayout(GridLayout):
   # display function
     def update_display(self, text):
@@ -38,22 +43,26 @@ class CalcGridlayout(GridLayout):
 
 
 #drop_down menu
-class Dropdown():
-    dropdown = DropDown()
-    for index in range(4):
-        btn = Button(text ='Value % d' % index, size_hint_y = None, height = 40)
-        btn.bind(on_release = lambda btn: dropdown.select(btn.text))
-        mainbutton = Button(text ='Hello', size_hint =(None, None), pos =(350, 300))
-        mainbutton.bind(on_release = dropdown.open)
-        dropdown.bind(on_select = lambda instance, x: setattr(mainbutton, 'text', x))
-        dropdown.add_widget(btn)
+#class Dropdown():
+   #dropdown = DropDown()
+    #for index in range(4):
+        #btn = Button(text ='Value % d' % index, size_hint_y = None, height = 40)
+        #btn.bind(on_release = lambda btn: dropdown.select(btn.text))
+        #mainbutton = Button(text ='Hello', size_hint =(None, None), pos =(350, 300))
+        #mainbutton.bind(on_release = dropdown.open)
+        #dropdown.bind(on_select = lambda instance, x: setattr(mainbutton, 'text', x))
+        #dropdown.add_widget(btn)
 
-
+#Length_Converter Display
 
 class ConverterGridlayout(GridLayout):
     def display(self):
         self.display.text+=text
 
+##Currency_converter Dispaly
+class CurrencyConverterGridlayout(GridLayout):
+    def display(self):
+        self.display.text+=text
 
 class CalculatorScreen(Screen):
     def __init__(self, **kwargs):
@@ -68,25 +77,36 @@ class ConverterScreen(Screen):
         layout = GridLayout(cols=1)
         layout.add_widget(ConverterGridlayout())
         self.add_widget(layout)
-        
+
+class CurrencyConverterScreen(Screen):
+    def __init__(self,**kwargs):
+        super(CurrencyConverterScreen,self).__init__(**kwargs)
+        layout = GridLayout(cols=1)
+        layout.add_widget(CurrencyConverterGridlayout())
+        self.add_widget(layout)        
        
 
 class MainScreen(Screen):
     pass
 
+#Home Screen
 class CalConApp(App):
     def build(self):
         sm = ScreenManager()
         sm.add_widget(MainScreen(name='main'))
         sm.add_widget(CalculatorScreen(name='calculator'))
         sm.add_widget(ConverterScreen(name='length_converter'))
+        sm.add_widget(CurrencyConverterScreen(name='currency_converter'))
         return sm
-
+#Home Screen navigation Buttons functions
     def switch_to_calculator(self):
         self.root.current = 'calculator'
 
     def switch_to_length_converter(self):
         self.root.current = 'length_converter'    
+
+    def switch_to_currency_converter(self):
+        self.root.current = 'currency_converter'
 
 if __name__ == '__main__':
     CalConApp().run()
